68a9420ec6daed3871b996c71e2396306b850fff
Michael Prieto <michaelpri10@gmail.com>
2015-12-27 21:33:59 -0500

    Only allow functions and methods to be called

--- command_functions/call_func_method.py
+++ command_functions/call_func_method.py
@@ -1,8 +1,11 @@
 from data_types import format_value
 
 def call_func_method(to_parse):
-    to_call = format_value(to_parse)
-    if to_call == False:
-        return False
+    if to_parse.startswith("function") or to_parse.find("method") != -1:
+        to_call = format_value(to_parse)
+        if to_call == False:
+            return False
+        else:
+            return "{0}".format(to_call)
     else:
-        return "{0}".format(to_call)
+        return False
